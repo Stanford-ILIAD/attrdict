a4c858adcc8930f3571b3b81a6deffd4c70b6b3e
Suneel Belkhale <neelus.genius@gmail.com>
2023-02-22 16:27:37 -0800

    license and pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name='AttrDict',
       version='1.0',
-      description='AttrDict and utilities',
+      description='AttrDict for nested dictionary processing and utilities',
       author='Suneel Belkhale',
       author_email='belkhale@stanford.edu',
       url='',
